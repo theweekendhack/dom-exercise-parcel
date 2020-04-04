c99e3368a70421e879e4fc58421e9f251f104ee0
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-04-04 15:57:30 -0400

    Completed project with a main function

--- js/app.js
+++ js/app.js
@@ -1,45 +1,42 @@
-//A CALL BACK FUNCTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
-/*
-    WHEN YOU PASS A FUNCITON AS AN ARGUMENT TO ANOTHER FUNCTION OR METHOD()
-*/
-function weightHandler()
+
+
+function  calCulateWightInLbs(kgs)
+{
+    return kgs*2.2;
+
+}
+
+function main()
 {
 
+    //fetching an element from the DOM
+    const button = document.querySelector("#calWeightButton");
 
-    /*Method 1: */
-    const txtWeight = document.querySelector("#personWeight");
-    const txtName = document.querySelector("#personName");
+
+    //Make the button an event listener, specifically listening to a click event
+    button.addEventListener("click",function(){
+
+        /*Method 1: */
+        const txtWeight = document.querySelector("#personWeight");
+        const txtName = document.querySelector("#personName");
+      
+
+        const weightInLbs = calCulateWightInLbs(parseFloat(txtWeight.value))
     
-    const weightInLbs = parseFloat(txtWeight.value) * 2.2;
-  
-    const  output = document.querySelector("#output");
-    output.innerHTML=`${txtName.value} your weight in lbs is ${weightInLbs.toFixed(2)}`
-    
-    txtWeight.value  = "";
-    txtName.value = "";
-    txtName.focus();
+        const  output = document.querySelector("#output");
+        output.innerHTML=`${txtName.value} your weight in lbs is ${weightInLbs.toFixed(2)}`
+        
+        txtWeight.value  = "";
+        txtName.value = "";
+        txtName.focus();
+        
+    });
 
     
+
 }
 
 
+main();
 
-//fetching an element from the DOM
-const button = document.querySelector("#calWeightButton");
-
-
-//Make the button an event listener, specifically listening to a click event
-button.addEventListener("click",weightHandler);
-
-
-addEventListener()
-
-
-
-
-
-
-/*
-    Who calls the  function is actually the addEvenListener method()
-*/
 
